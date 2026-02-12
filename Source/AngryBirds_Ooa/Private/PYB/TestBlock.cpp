414e16f2afca3b9a7e99f45517e18837bcea4913
Yebin11 <55072785+Yebin11@users.noreply.github.com>
2026-02-12 18:04:58 +0900

    중력 추가, addforce 구현중

--- Source/AngryBirds_Ooa/Private/PYB/TestBlock.cpp
+++ Source/AngryBirds_Ooa/Private/PYB/TestBlock.cpp
@@ -2,13 +2,22 @@
 
 
 #include "PYB/TestBlock.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Materials/MaterialExpressionSubstrate.h"
 
 // Sets default values
 ATestBlock::ATestBlock()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	DefaultSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneComp"));
+	RootComponent = DefaultSceneComp;
+	
+	bodyMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMeshComp"));
+	bodyMeshComp->SetupAttachment(DefaultSceneComp);
+	bodyMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//bodyMeshComp->SetRelativeScale3D(FVector(0.25f));
 }
 
 // Called when the game starts or when spawned
@@ -16,12 +25,65 @@ void ATestBlock::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	InitConstant();
 }
 
 // Called every frame
 void ATestBlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
+	AddGravity(DeltaTime);
+	
+	//CurVelocity.X = FMath::FInterpTo(CurVelocity.X, 0.0f, DeltaTime, 2.0f);
+	//CurVelocity.X = FMath::FInterpTo(CurVelocity.Y, 0.0f, DeltaTime, 2.0f);
+	
+	FVector NewLocation = GetActorLocation() + (CurVelocity * DeltaTime);
+	SetActorLocation(NewLocation, true);
 }
 
+void ATestBlock::InitConstant()
+{
+	InAngle = 0.0f;
+	Mass = 100.0f;
+	MuStop = 0.0f;
+	MuMove = 0.0f;
+	Gravity = -980.0f;
+	CurVelocity = FVector::ZeroVector;
+	
+	CheckZ = 3.0f;
+	BoxZ = bodyMeshComp->Bounds.BoxExtent.Z;
+	
+	bIsGround = CheckGround();
+}
+
+void ATestBlock::AddGravity(float dt)
+{
+	bIsGround = CheckGround();
+	if (bIsGround)
+	{
+		CurVelocity.Z = 0;
+		return;
+	}
+	
+	CurVelocity.Z += Gravity * dt;
+	
+	// CurVelocity += Gravity * dt;
+	// FVector NewLocation = GetActorLocation() + FVector(0, 0, VerticalVelocity * dt);
+	// SetActorLocation(NewLocation, true);
+}
+
+bool ATestBlock::CheckGround()
+{
+	FHitResult hitInfo;
+	FCollisionQueryParams params;
+	FVector centerPos = bodyMeshComp->GetComponentLocation();
+	FVector floorPos = centerPos - BoxZ - CheckZ;
+		
+	bool bHit = GetWorld()->LineTraceSingleByChannel(hitInfo, centerPos, floorPos, ECC_Visibility, params);
+	return bHit;
+}
+
+void ATestBlock::AddForceVector(FVector InputForce)
+{
+	CurVelocity += InputForce;
+}
--- Source/AngryBirds_Ooa/Public/PYB/TestBlock.h
+++ Source/AngryBirds_Ooa/Public/PYB/TestBlock.h
@@ -22,5 +22,43 @@ protected:
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	
+public:
+	// 컴포넌트 설정
+	UPROPERTY(VisibleAnywhere, Category=Scene)
+	class USceneComponent* DefaultSceneComp;
+	UPROPERTY(EditAnywhere, Category=BodyMesh)
+	class UStaticMeshComponent* bodyMeshComp;
+	
+	// 컴포넌트 관련 변수 선언
+	float BoxZ;
+	float CheckZ;
+	
+	// 물리 변수 선언
+	UPROPERTY(EditAnywhere, Category="Physics") float InAngle;
+	UPROPERTY(EditAnywhere, Category="Physics") float Mass;
+	UPROPERTY(EditAnywhere, Category="Physics") float MuStop;
+	UPROPERTY(EditAnywhere, Category="Physics") float MuMove;
+	UPROPERTY(EditAnywhere, Category="Physics") float Gravity;
+	UPROPERTY(EditAnywhere, Category="Physics") FVector CurVelocity;
+	
+	// 상태 변수 선언
+	UPROPERTY(EditAnywhere, Category="Physics") bool bIsGround;
+	// bool bUseGravity;
+	
+	// 실제 작용하는 힘 선언
+	FVector FTotal;
+	FVector FImpact;
+	FVector Velocity;
 
+	// 초기 상태 설정 함수
+	void InitConstant();
+	
+	// 움직임 관련 함수
+	void AddGravity(float DeltaSeconds);
+	UFUNCTION(BlueprintCallable, Category="Physics")
+	void AddForceVector(FVector InputForce);
+	
+	// 상태 관련 함수
+	bool CheckGround();
 };
