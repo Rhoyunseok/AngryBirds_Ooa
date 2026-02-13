33b4629b0f473c1161c2ae0d67cddb522602e678
Yebin11 <55072785+Yebin11@users.noreply.github.com>
2026-02-13 18:03:27 +0900

    중력, 마찰력 수정

--- Source/AngryBirds_Ooa/Private/PYB/TestBlock.cpp
+++ Source/AngryBirds_Ooa/Private/PYB/TestBlock.cpp
@@ -1,9 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+
+#include "Components/BoxComponent.h"
 #include "PYB/TestBlock.h"
-#include "Kismet/KismetSystemLibrary.h"
-#include "Materials/MaterialExpressionSubstrate.h"
+
+#define MAX_MOVE_SPEED 500
+#define MAX_FALL_SPEED 100
 
 // Sets default values
 ATestBlock::ATestBlock()
@@ -11,13 +14,16 @@ ATestBlock::ATestBlock()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 	
-	DefaultSceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneComp"));
-	RootComponent = DefaultSceneComp;
+	// 콜리전
+	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxCollisionComp"));
+	CollisionComp->SetCollisionProfileName(TEXT("BlockAll"));
+	RootComponent = CollisionComp;
 	
-	bodyMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMeshComp"));
-	bodyMeshComp->SetupAttachment(DefaultSceneComp);
-	bodyMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	//bodyMeshComp->SetRelativeScale3D(FVector(0.25f));
+	// 메쉬
+	BodyMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyMeshComp"));
+	BodyMeshComp->SetupAttachment(CollisionComp);
+	BodyMeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	//BodyMeshComp->SetRelativeScale3D(FVector(0.25f));
 }
 
 // Called when the game starts or when spawned
@@ -26,64 +32,140 @@ void ATestBlock::BeginPlay()
 	Super::BeginPlay();
 	
 	InitConstant();
+	
+	
+	CurVelocity = FVector(1000, 1000, 0);
+	bIsMoving = true;
 }
 
 // Called every frame
 void ATestBlock::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	AddGravity(DeltaTime);
+
+	// 현재 상태 체크
+	// bIsGround = CheckGround().bBlockingHit;
+	// bIsMoving = CheckMoving();
+	if (!bIsGround)
+	{
+		GroundHitResult = CheckGround();
+		if (!GroundHitResult.bBlockingHit)
+		{
+			AddGravity(DeltaTime);
+		} else
+		{
+			SetActorGround();
+		}
+	}
+	if (bIsMoving && bIsGround)
+	{
+		AddFriction(DeltaTime);
+	}
+	
+	ApplyCurVelocity();
+	UpdateActorLocation();
+	
+	// 현재 속도 변경
+	// if (bIsGround) AddGravity(DeltaTime);
+	// if (bIsMoving) AddFriction(DeltaTime);
 	
 	//CurVelocity.X = FMath::FInterpTo(CurVelocity.X, 0.0f, DeltaTime, 2.0f);
 	//CurVelocity.X = FMath::FInterpTo(CurVelocity.Y, 0.0f, DeltaTime, 2.0f);
 	
-	FVector NewLocation = GetActorLocation() + (CurVelocity * DeltaTime);
-	SetActorLocation(NewLocation, true);
+	// FVector NewLocation = GetActorLocation() + (CurVelocity * DeltaTime);
+	// SetActorLocation(NewLocation, true);
 }
 
 void ATestBlock::InitConstant()
 {
-	InAngle = 0.0f;
-	Mass = 100.0f;
-	MuStop = 0.0f;
-	MuMove = 0.0f;
-	Gravity = -980.0f;
+	//InAngle = 0.0f;
+	Mass = 1.f;
+	// MuStop = 0.9f;
+	MuMove = 0.5f;
+	Gravity = -9.8f;
 	CurVelocity = FVector::ZeroVector;
 	
-	CheckZ = 3.0f;
-	BoxZ = bodyMeshComp->Bounds.BoxExtent.Z;
+	BoxZ = CollisionComp->Bounds.BoxExtent.Z;
 	
-	bIsGround = CheckGround();
+	bIsGround = false;
+	bIsMoving = false;
 }
 
-void ATestBlock::AddGravity(float dt)
+void ATestBlock::AddGravity(float DeltaSeconds)
 {
-	bIsGround = CheckGround();
-	if (bIsGround)
-	{
-		CurVelocity.Z = 0;
-		return;
-	}
+	CurVelocity.Z += Gravity * DeltaSeconds;
 	
-	CurVelocity.Z += Gravity * dt;
+	
+	
 	
 	// CurVelocity += Gravity * dt;
 	// FVector NewLocation = GetActorLocation() + FVector(0, 0, VerticalVelocity * dt);
 	// SetActorLocation(NewLocation, true);
 }
 
-bool ATestBlock::CheckGround()
+// void ATestBlock::AddFriction(float DeltaSeconds)
+// {
+// 	if (CurVelocity.X > 0.1)
+// 	{
+// 		CurVelocity.X -= MuStop * DeltaSeconds;
+// 	}
+// 	if (CurVelocity.Y > 0.1)
+// 	{
+// 		CurVelocity.Y -= MuStop * DeltaSeconds;
+// 	}
+// }
+
+FHitResult ATestBlock::CheckGround()
 {
 	FHitResult hitInfo;
 	FCollisionQueryParams params;
-	FVector centerPos = bodyMeshComp->GetComponentLocation();
-	FVector floorPos = centerPos - BoxZ - CheckZ;
-		
-	bool bHit = GetWorld()->LineTraceSingleByChannel(hitInfo, centerPos, floorPos, ECC_Visibility, params);
-	return bHit;
+	FVector startPos = CollisionComp->GetComponentLocation();
+	FVector endPos = startPos - BoxZ - 5.f;
+	
+	params.AddIgnoredActor(this);
+	GetWorld()->LineTraceSingleByChannel(hitInfo, startPos, endPos, ECC_Visibility, params);
+	return hitInfo;
 }
 
-void ATestBlock::AddForceVector(FVector InputForce)
+// bool ATestBlock::CheckMoving()
+// {
+// 	return CurVelocity.Size2D() == 0;
+// }
+
+//void ATestBlock::AddForceVector(FVector InputForce)
+// {
+// 	CurVelocity += InputForce;
+// }
+
+void ATestBlock::SetActorGround()
 {
-	CurVelocity += InputForce;
+	CurVelocity.Z = 0;
+	FVector NewLocation = FVector(GetActorLocation().X, GetActorLocation().Y, GroundHitResult.ImpactNormal.Z + BoxZ);
+	SetActorLocation(NewLocation);
+	bIsGround = true;
+}
+
+void ATestBlock::ApplyCurVelocity()
+{	
+	FVector VelXY = FVector(CurVelocity.X, CurVelocity.Y, 0.f);
+	VelXY = VelXY.GetClampedToMaxSize(MAX_MOVE_SPEED);
+	float VelZ = CurVelocity.Z < MAX_FALL_SPEED ? CurVelocity.Z : MAX_FALL_SPEED;
+	CurVelocity = VelXY;
+	CurVelocity.Z = VelZ;
+	//CurVelocity = CurVelocity.GetClampedToMaxSize(MAX_SPEED);
+}
+
+void ATestBlock::UpdateActorLocation()
+{
+	AddActorWorldOffset(CurVelocity/10, false);
+}
+
+void ATestBlock::AddFriction(float DeltaSeconds)
+{
+	FVector FDirection = -CurVelocity.GetSafeNormal2D();
+	CurVelocity.X += FDirection.X * MuMove * Mass * -Gravity;
+	CurVelocity.Y += FDirection.Y * MuMove * Mass * -Gravity;
+	if (FDirection.X < 0 == CurVelocity.X < 0) CurVelocity.X = 0;
+	if (FDirection.Y < 0 == CurVelocity.Y < 0) CurVelocity.Y = 0;
+	if (CurVelocity.Size2D() == 0) bIsMoving = false;
 }
--- Source/AngryBirds_Ooa/Public/PYB/TestBlock.h
+++ Source/AngryBirds_Ooa/Public/PYB/TestBlock.h
@@ -25,40 +25,49 @@ public:
 	
 public:
 	// 컴포넌트 설정
-	UPROPERTY(VisibleAnywhere, Category=Scene)
-	class USceneComponent* DefaultSceneComp;
+	UPROPERTY(VisibleAnywhere, Category=Collision)
+	class UBoxComponent* CollisionComp;
 	UPROPERTY(EditAnywhere, Category=BodyMesh)
-	class UStaticMeshComponent* bodyMeshComp;
+	class UStaticMeshComponent* BodyMeshComp;
 	
 	// 컴포넌트 관련 변수 선언
 	float BoxZ;
-	float CheckZ;
 	
 	// 물리 변수 선언
-	UPROPERTY(EditAnywhere, Category="Physics") float InAngle;
+	//UPROPERTY(EditAnywhere, Category="Physics") float InAngle;
 	UPROPERTY(EditAnywhere, Category="Physics") float Mass;
-	UPROPERTY(EditAnywhere, Category="Physics") float MuStop;
+	// UPROPERTY(EditAnywhere, Category="Physics") float MuStop;
 	UPROPERTY(EditAnywhere, Category="Physics") float MuMove;
 	UPROPERTY(EditAnywhere, Category="Physics") float Gravity;
-	UPROPERTY(EditAnywhere, Category="Physics") FVector CurVelocity;
 	
 	// 상태 변수 선언
-	UPROPERTY(EditAnywhere, Category="Physics") bool bIsGround;
+	UPROPERTY(EditAnywhere, Category="State") bool bIsGround;
+	UPROPERTY(EditAnywhere, Category="State") bool bIsMoving;
 	// bool bUseGravity;
 	
+	// 운동 관련 변수 선언
+	UPROPERTY(EditAnywhere, Category="Movement") FVector CurVelocity;
+	UPROPERTY(EditAnywhere, Category="Movement") FHitResult GroundHitResult;
+	
+	
 	// 실제 작용하는 힘 선언
-	FVector FTotal;
-	FVector FImpact;
-	FVector Velocity;
+	// FVector FTotal;
+	// FVector FImpact;
+	// FVector Velocity;
 
 	// 초기 상태 설정 함수
-	void InitConstant();
+	virtual void InitConstant();
 	
 	// 움직임 관련 함수
 	void AddGravity(float DeltaSeconds);
-	UFUNCTION(BlueprintCallable, Category="Physics")
-	void AddForceVector(FVector InputForce);
+	void AddFriction(float DeltaSeconds);
+	// UFUNCTION(BlueprintCallable, Category="Movement")
+	// void AddForceVector(FVector InputForce);
+	void SetActorGround();
+	void ApplyCurVelocity();
+	void UpdateActorLocation();
 	
 	// 상태 관련 함수
-	bool CheckGround();
+	FHitResult CheckGround();
+	// bool CheckMoving();
 };
